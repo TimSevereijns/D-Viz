15ebeb5bba8b0f13886558d7393982ee2771b56f
Tim Severeijns <tim.severeijns@gmail.com>
2015-03-11 22:11:39 -0700

    Squarify
    + Switched gears to an iterative approach. All the pieces are in place,
    I think, but I have yet to test it.

--- Visualizations/squarifiedTreemap.cpp
+++ Visualizations/squarifiedTreemap.cpp
@@ -12,12 +12,12 @@ namespace
     * @param[in] row                The items to be placed in the current piece of real estate.
     * @param[in] realEsate          The space into which the nodes have to be placed.
     */
-   void LayoutStrip(std::vector<TreeNode<VizNode>>& row, RealEstate& realEstate)
+   void LayoutStrip(std::vector<TreeNode<VizNode>>& row, Block& realEstate)
    {
       static const float BLOCK_HEIGHT = 0.0625f;
       static const float BLOCK_TO_REAL_ESTATE_RATIO = 0.9f;
 
-      if (row.size() == 0 || !realEstate.m_block.IsDefined())
+      if (row.size() == 0 || !realEstate.IsDefined())
       {
          return;
       }
@@ -43,53 +43,53 @@ namespace
          const float percentageOfParent = static_cast<float>(fileSize) /
                static_cast<float>(parentFileSize);
 
-         if (realEstate.m_block.m_width > realEstate.m_block.m_depth)
+         if (realEstate.m_width > realEstate.m_depth)
          {
-            const auto paddedBlockWidth = realEstate.m_block.m_width * percentageOfParent;
+            const auto paddedBlockWidth = realEstate.m_width * percentageOfParent;
             const auto actualBlockWidth = paddedBlockWidth * BLOCK_TO_REAL_ESTATE_RATIO;
-            const auto widthPaddingPerSide = ((realEstate.m_block.m_width * 0.1f) / nodeCount) / 2.0f;
+            const auto widthPaddingPerSide = ((realEstate.m_width * 0.1f) / nodeCount) / 2.0f;
 
-            const auto actualBlockDepth = realEstate.m_block.m_depth * BLOCK_TO_REAL_ESTATE_RATIO;
-            const auto depthPaddingPerSide = (realEstate.m_block.m_height - actualBlockDepth) / 2.0f;
+            const auto actualBlockDepth = realEstate.m_depth * BLOCK_TO_REAL_ESTATE_RATIO;
+            const auto depthPaddingPerSide = (realEstate.m_height - actualBlockDepth) / 2.0f;
 
             const auto offset = QVector3D(
-               (realEstate.m_block.m_width * percentCovered) + widthPaddingPerSide,
+               (realEstate.m_width * percentCovered) + widthPaddingPerSide,
                BLOCK_HEIGHT,
                -depthPaddingPerSide
             );
 
-            data.m_block = Block(realEstate.m_block.m_vertices[0] + offset,
+            data.m_block = Block(realEstate.m_vertices[0] + offset,
                actualBlockWidth,
                BLOCK_HEIGHT,
                actualBlockDepth
             );
 
             additionalCoverage = (actualBlockWidth + (2 * widthPaddingPerSide)) /
-                  realEstate.m_block.m_width;
+                  realEstate.m_width;
          }
          else
          {
-            const auto paddedBlockDepth = realEstate.m_block.m_depth * percentageOfParent;
+            const auto paddedBlockDepth = realEstate.m_depth * percentageOfParent;
             const auto actualBlockDepth = paddedBlockDepth * BLOCK_TO_REAL_ESTATE_RATIO;
-            const auto depthPaddingPerSide = ((realEstate.m_block.m_depth * 0.1f) / nodeCount) / 2.0f;
+            const auto depthPaddingPerSide = ((realEstate.m_depth * 0.1f) / nodeCount) / 2.0f;
 
-            const auto actualBlockWidth = realEstate.m_block.m_width * BLOCK_TO_REAL_ESTATE_RATIO;
-            const auto widthPaddingPerSide = (realEstate.m_block.m_height - actualBlockWidth) / 2.0f;
+            const auto actualBlockWidth = realEstate.m_width * BLOCK_TO_REAL_ESTATE_RATIO;
+            const auto widthPaddingPerSide = (realEstate.m_height - actualBlockWidth) / 2.0f;
 
             const auto offset = QVector3D(
                widthPaddingPerSide,
                BLOCK_HEIGHT,
-               -(realEstate.m_block.m_depth * percentCovered) - depthPaddingPerSide
+               -(realEstate.m_depth * percentCovered) - depthPaddingPerSide
             );
 
-            data.m_block = Block(realEstate.m_block.m_vertices[0] + offset,
+            data.m_block = Block(realEstate.m_vertices[0] + offset,
                actualBlockWidth,
                BLOCK_HEIGHT,
                actualBlockDepth
             );
 
             additionalCoverage = (actualBlockDepth + (2 * depthPaddingPerSide)) /
-                  realEstate.m_block.m_depth;
+                  realEstate.m_depth;
          }
 
          if (additionalCoverage)
@@ -97,7 +97,7 @@ namespace
             assert(!"The new block does not appear to have required dimensions!");
          }
 
-         realEstate.m_block.m_percentCovered += additionalCoverage;
+         realEstate.m_percentCovered += additionalCoverage;
       });
    }
 
@@ -151,52 +151,35 @@ namespace
          (areaSquared) / (lengthSquared * smallestElement));
    }
 
-   /**
-    * @brief Squarify is the main recursive function that drives the creation of the Squarifed map.
-    * @param[in] children           The remaining child nodes that need to be laid out.
-    * @param[in] realEstate         The space available into which the children can be laid out.
-    */
-   void Squarify(TreeNode<VizNode>& children, std::vector<TreeNode<VizNode>>& row,
-      RealEstate& realEstate)
+   void IterativeSquarify(TreeNode<VizNode>& children)
    {
-      TreeNode<VizNode>& firstChild = children;
+      TreeNode<VizNode>* currentChild = &children;
+      std::vector<TreeNode<VizNode>> currentRow;
 
-      const float shortestSide = std::min(realEstate.m_block.m_height, realEstate.m_block.m_depth);
-
-      // If worst aspect ratio improves, add block to current row:
-      if (ComputeWorstAspectRatio(row, nullptr, shortestSide) <=
-          ComputeWorstAspectRatio(row, &firstChild, shortestSide))
+      while (currentChild)
       {
-         RealEstate remainingRealEstate;
-         // TODO: Compute remaining real estate.
+         TreeNode<VizNode>* parentNode = &*currentChild->GetParent();
+         assert(parentNode);
 
-         row.emplace_back(firstChild);
-         Squarify(*children.GetNextSibling(), row, remainingRealEstate);
+         const float shortestSide =
+            std::min(parentNode->GetData().m_block.m_height, parentNode->GetData().m_block.m_depth);
+
+         // If worst aspect ratio improves, add block to current row:
+         if (ComputeWorstAspectRatio(currentRow, currentChild, shortestSide) <
+             ComputeWorstAspectRatio(currentRow, nullptr, shortestSide))
+         {
+            currentRow.emplace_back(*currentChild);
+         }
+         else // if aspect ratio gets worse, create a new row:
+         {
+            LayoutStrip(currentRow, parentNode->GetData().m_block);
+
+            currentRow.clear();
+            currentRow.emplace_back(*currentChild);
+         }
+
+         currentChild = &*currentChild->GetNextSibling();
       }
-      else // if aspect ratio gets worse, create a new row:
-      {
-         LayoutStrip(row, realEstate);
-
-         RealEstate freshRealEstate;
-         // TODO: Compute new real estate.
-
-         Squarify(*children.GetNextSibling(), std::vector<TreeNode<VizNode>>(), freshRealEstate);
-      }
-   }
-
-   /**
-    * @brief ParseNode kicks off the squarification of each node.
-    * @param node[in]               The current node being parsed.
-    */
-   void ParseNode(TreeNode<VizNode>& node, DiskScanner& scanner)
-   {
-      Block parentBlock = node.GetParent()
-         ? node.GetParent()->GetData().m_block
-         : Block(QVector3D(0, 0, 0), 10.0f, 0.125f, 10.0f); // The dummy base node.
-
-      RealEstate realEstate(parentBlock, scanner.GetDirectoryTree().GetHead()->GetData().m_file.m_size);
-
-      Squarify(*node.GetFirstChild(), std::vector<TreeNode<VizNode>>(), realEstate);
    }
 }
 
@@ -220,5 +203,8 @@ void SquarifiedTreeMap::ParseScan()
          { return lhs.GetData().m_file.m_size < rhs.GetData().m_file.m_size; });
    });
 
-   //std::for_each(tree.beginPreOrder(), tree.endPreOrder(), ParseNode);
+   // Set the size of the root visualization:
+   tree.GetHead()->GetData().m_block = Block(QVector3D(0, 0, 0), 10.0f, 0.125f, 10.0f);
+
+   IterativeSquarify(*tree.GetHead()->GetFirstChild());
 }
