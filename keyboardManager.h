4a8c72cf452250cbd32b5617f1e7c19cec1d56e8
Tim Severeijns <tim.severeijns@gmail.com>
2015-05-03 22:06:52 -0700

    More Documentation
    + Added some more documentation to the Keyboard Manager class.

--- keyboardManager.h
+++ keyboardManager.h
@@ -17,11 +17,29 @@ class KeyboardManager
       KeyboardManager();
       ~KeyboardManager();
 
+      /**
+       * @brief IsKeyDown indicates whether the key in question is currently
+       * in a pressed state (according to the keyboard manager).
+       * 
+       * @param[in] key                The key whose state to check.
+       * 
+       * @returns true if the key is currently down; false otherwise.
+       */
       bool IsKeyDown(const Qt::Key key);
+      
+      /**
+      * @brief IsKeyUp indicates whether the key in question is currently
+      * not in a pressed state.
+      * 
+      * @param[in] key                 The key whose state to check.
+      * 
+      * @returns true if the key is currently up; false otherwise.
+      */
       bool IsKeyUp(const Qt::Key key);
 
       /**
        * @brief UpdateKeyState Records the state of the specified key.
+       * 
        * @param key[in]                The key to be updated.
        * @param state[in]              The state of the key.
        */
