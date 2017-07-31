87102653ef6a5cdf66f318ca04ccbf54d9aa1bd8
Tim Severeijns <tim.severeijns@gmail.com>
2017-07-30 18:59:31 -0700

    Gamepad Support on Linux
    + Using the QGamepad class, Xbox controller support should now exist in both Windows and Linux.
    + Fixed all GCC warnings (under the -Wall flag).

---
+++ source/HID/gamepad.cpp
@@ -0,0 +1,6 @@
+#include "gamepadMonitor.h"
+
+GamepadMonitor::GamepadMonitor()
+{
+
+}
---
+++ source/HID/gamepad.h
@@ -0,0 +1,11 @@
+#ifndef GAMEPADMONITOR_H
+#define GAMEPADMONITOR_H
+
+
+class GamepadMonitor
+{
+public:
+    GamepadMonitor();
+};
+
+#endif // GAMEPADMONITOR_H
