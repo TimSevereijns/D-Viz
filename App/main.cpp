8253a2fafe3b9c4150ab5837359b15754a535902
Tim Severeijns <tim.severeijns@gmail.com>
2019-11-09 15:15:31 -0800

    Added Linux Iconography
    + See https://stackoverflow.com/a/38100598/694056 for information on associating an icon with an application.

--- App/main.cpp
+++ App/main.cpp
@@ -13,6 +13,8 @@ int main(int argc, char* argv[])
 
 #if defined(Q_OS_WIN)
     application.setWindowIcon(QIcon{ "Icons/Windows/D-Viz.ico" });
+#elif defined(Q_OS_LINUX)
+    application.setWindowIcon(QIcon{ "Icons/Linux/32x32/D-Viz.png" });
 #endif
 
     Controller controller{};
