e3f353d3f44a7fa0b51fd0880ccfae42043c2a9a
Tim Severeijns <tim.severeijns@gmail.com>
2020-09-17 20:07:05 -0700

    Revamped the About Dialog
    + Might still want to add a large icon to the dialog to spruce it up a bit.

--- Source/View/Dialogs/aboutDialog.cpp
+++ Source/View/Dialogs/aboutDialog.cpp
@@ -4,6 +4,6 @@ AboutDialog::AboutDialog(QWidget* parent) : QDialog{ parent }, m_ui{}
 {
     m_ui.setupUi(this);
 
-    const auto timeStamp = QString{ __DATE__ } + ", " + QString{ __TIME__ }; // NOLINT
-    m_ui.timeStampLabel->setText(timeStamp);
+    const auto timeStamp = QString{ __DATE__ } + ", " + QString{ __TIME__ };
+    m_ui.timestamp->setText("Built on " + timeStamp + ".");
 }
